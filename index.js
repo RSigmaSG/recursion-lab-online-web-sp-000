e65cda99a99ffa291b28d277a95f1947985f5a7b
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-26 05:44:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,116 @@
 // Code your solution here!
 
+
+function printString(inputString)
+{
+  let substring;
+  
+  console.log(inputString[0]);
+  if (inputString.length > 1)
+  {
+    substring = inputString.substring(1, inputString.length);
+    printString(substring);
+  }
+  else {
+    return true;
+  }
+  
+}
+
+// Code your solution here!
+
+
+function reverseString(inputString)
+{
+  let substring;
+  
+  if (inputString === "")
+  {
+    return "";
+  }  
+  else
+  {
+    inputString = reverseString(inputString.substring(1)) + inputString.charAt(0);
+  }
+  return inputString;
+}
+
+function isPalindrome(inputString)
+{
+  let substring;
+  let test = false;
+  
+  if (inputString === "" || inputString.length ===1)
+  {
+    return true;
+  }  
+  else
+  {
+    if(inputString.charAt(0) === inputString.charAt(inputString.length - 1))
+    {
+      test = isPalindrome(inputString.substring(1,inputString.length - 2));
+      test = true;
+    }
+  }
+  return test;
+}
+
+function addUpTo(inputArray, inputIndex)
+{
+  
+  if (inputIndex === 0)
+  {
+    return inputArray[inputIndex];
+  }
+  else
+  {
+    return (inputArray[inputIndex] + addUpTo(inputArray, inputIndex - 1));
+  }
+  
+}
+
+function maxOf(inputArray)
+{
+  
+  if(inputArray.length === 1)
+  {    
+    
+    return inputArray[0];
+    
+  }
+  else
+  {
+    let max = maxOf(inputArray.slice(1));
+    if (inputArray[0] < max)
+    {
+      return max;
+    }
+    else
+    {
+      return inputArray[0];
+    }
+  }
+
+}
+
+function includesNumber(inputArray, inputElement)
+{
+  
+  if(inputArray[0] === inputElement)
+  {
+    //console.log(`${inputElement} was found`)
+    return true
+  }
+  else if(inputArray.length != 1)
+  {
+    //console.log(`${inputArray} does not have ${inputElement} so far`)
+    return includesNumber(inputArray.slice(1),inputElement)
+  }
+  else
+  {
+    //console.log(`${inputElement} not found :(((`)
+    return false
+    
+  }
+
+}
